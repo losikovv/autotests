3ea0889a4ee17b8163fb508fd6f77aa75758f9b2
Stanislav Klimov <stanislav.klimov@instamart.ru>
2018-01-25 11:31:57 +0300

    PageNavHelpers introduced

---
+++ web-tests/src/test/java/ru/instamart/autotests/appmanager/LandingPageNavHelper.java
@@ -0,0 +1,14 @@
+package ru.instamart.autotests.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+// помощник навигации на лендинге
+// тут описыввем локаторы элементов на странице
+public class LandingPageNavHelper extends NavigationHelper {
+    public LandingPageNavHelper(WebDriver driver) {
+        super(driver);
+    }
+
+    //
+
+}
--- web-tests/src/test/java/ru/instamart/autotests/appmanager/NavigationHelper.java
+++ web-tests/src/test/java/ru/instamart/autotests/appmanager/NavigationHelper.java
@@ -3,6 +3,8 @@ package ru.instamart.autotests.appmanager;
 import org.openqa.selenium.WebDriver;
 import ru.instamart.autotests.models.RetailerData;
 
+// в этом классе только глобальная навигация по прямым ссылкам
+// навигация по страницам описываем в классах-наследниках (PageNavHelpers)
 public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver driver) {
