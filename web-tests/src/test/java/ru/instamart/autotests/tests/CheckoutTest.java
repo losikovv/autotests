e28338683bbcdd501ae63ad440cd6d85eb100016
Stanislav Klimov <stanklimov@yandex.ru>
2018-04-16 13:41:03 +0300

    Checkout test created

---
+++ web-tests/src/test/java/ru/instamart/autotests/tests/CheckoutTest.java
@@ -0,0 +1,21 @@
+package ru.instamart.autotests.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckoutTest extends TestBase {
+
+    @Test(priority = 1)
+    public void makeTestOrder() throws Exception {
+        app.getNavigationHelper().getLandingPage();
+        app.getSessionHelper().doLoginAsReturningCustomer();
+        Assert.assertTrue(app.getSessionHelper().isUserAuthorised(), "User wasn't successfully authorised"+"\n");
+        app.getShoppingHelper().openCart();
+        app.getShoppingHelper().proceedToCheckout();
+        app.getCheckoutHelper().completeCheckout("cash");
+        //TODO добавить проверку на активность заказа с помощью метода isOrderActive
+        app.getProfileHelper().cancelLastOrder();
+        //TODO добавить проверку на отмененность заказа с помощью метода isOrderActive
+    }
+
+}
--- web-tests/src/test/java/ru/instamart/autotests/tests/ShoppingTest.java
+++ web-tests/src/test/java/ru/instamart/autotests/tests/ShoppingTest.java
@@ -27,17 +27,4 @@ public class ShoppingTest extends TestBase {
 
     */
 
-    @Test(priority = 1)
-    public void makeTestOrder() throws Exception {
-        app.getNavigationHelper().getLandingPage();
-        app.getSessionHelper().doLoginAsReturningCustomer();
-        Assert.assertTrue(app.getSessionHelper().isUserAuthorised(), "User wasn't successfully authorised"+"\n");
-        app.getShoppingHelper().openCart();
-        app.getShoppingHelper().proceedToCheckout();
-        app.getCheckoutHelper().completeCheckout("cash");
-        //TODO добавить проверку на активность заказа с помощью метода isOrderActive
-        app.getProfileHelper().cancelLastOrder();
-        //TODO добавить проверку на отмененность заказа с помощью метода isOrderActive
-    }
-
 }
