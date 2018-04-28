68006bcd43c999153f9e9f1d525ac9929eafcb3b
Stanislav Klimov <stanklimov@yandex.ru>
2018-04-28 14:52:40 +0300

    second сleanup test added

--- web-tests/src/test/java/ru/instamart/autotests/tests/Cleanup.java
+++ web-tests/src/test/java/ru/instamart/autotests/tests/Cleanup.java
@@ -4,15 +4,22 @@ import org.testng.annotations.Test;
 
 public class Cleanup extends TestBase {
 
-    @Test(description = "Отмена всех тестовых заказов")
+    @Test(description = "Отмена всех тестовых заказов", priority = 1)
     public void cleanupTestOrders() throws Exception {
         app.getSessionHelper().cancelAllTestOrders();
         assertNoTestOrdersLeftActive();
     }
 
-    @Test(description = "Удаление всех тестовых юзеров",priority = 1)
+    @Test(description = "Удаление всех тестовых юзеров", priority = 2)
     public void cleanupTestUsers() throws Exception {
         app.getSessionHelper().deleteAllTestUsers();
+        assertNoTestUsersLeft();
+    }
+
+    @Test(description = "Тест метода cleanup (отмена заказов и удаление юзеров)", priority = 3)
+    public void cleanupTest() throws Exception {
+        app.getSessionHelper().cleanup();
+        assertNoTestOrdersLeftActive();
         assertNoTestUsersLeft();
     }
 
