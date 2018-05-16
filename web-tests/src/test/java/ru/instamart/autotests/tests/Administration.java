342680d12b93c22a986a7447272d4631657eeba6
Stanislav Klimov <stanklimov@yandex.ru>
2018-05-16 14:36:21 +0300

    admin panel tests added to test suites

--- web-tests/src/test/java/ru/instamart/autotests/tests/Administration.java
+++ web-tests/src/test/java/ru/instamart/autotests/tests/Administration.java
@@ -13,40 +13,45 @@ import org.testng.annotations.Test;
 public class Administration extends TestBase {
 
 
+    // TODO Кейс на недоступность админки пользователем без админ-прав
+
+
     @BeforeMethod(alwaysRun = true)
     public void reachAdministrationPanel(){
         app.getSessionHelper().getUrlAsAdmin("https://instamart.ru/admin/shipments");
     }
 
 
-    // TODO Кейс на недоступность админки пользователем без админ-прав
-
-
-    @Test(priority = 801)
+    @Test(
+            description = "Тест возобновления заказа через админку",
+            groups = {"acceptance","regression"},
+            priority = 701
+    )
     public void resumeOrder() throws Exception {
-        //TODO убрать хардкод!
-        String orderNumber = "R124857258";
-        // идем на лендинг
-        app.getNavigationHelper().getLandingPage();
-        // логинимся
-        app.getSessionHelper().doLoginAsAdmin();
-        // проверяем что авторизованы
-        Assert.assertTrue(app.getSessionHelper().isUserAuthorised(), "User wasn't successfully authorised"+"\n");
-        // идем на страницу заказа в админке
+
+        String orderNumber = "R124857258"; //TODO убрать хардкод номера заказа, делать новый тестовый заказ перед тестами
         app.getNavigationHelper().getOrderAdminPage(orderNumber);
-        // проверяем что заказ отменен
-        Assert.assertTrue(app.getSessionHelper().isOrderCanceled(),"The order is already active" + "\n");
-        // возобновляем заказ
+
+        // Assert order is cancelled
+        Assert.assertTrue(app.getSessionHelper().isOrderCanceled(),
+                "The order is already active" + "\n");
+
         app.getSessionHelper().resumeOrder();
-        // проверяем что заказ возобновлен
-        Assert.assertFalse(app.getSessionHelper().isOrderCanceled(),"The order wasn't resumed" + "\n");
+
+        // Assert order isn't cancelled
+        Assert.assertFalse(app.getSessionHelper().isOrderCanceled(),
+                "The order wasn't resumed" + "\n");
     }
 
 
-    @Test(priority = 802)
+    @Test(
+            description = "Тест отмены заказа через админку",
+            groups = {"acceptance","regression"},
+            priority = 702
+    )
     public void cancelOrder() throws Exception {
-        //TODO убрать хардкод номера заказа, делать новый тестовый заказ перед тестами
-        String orderNumber = "R124857258";
+
+        String orderNumber = "R124857258"; //TODO убрать хардкод номера заказа, делать новый тестовый заказ перед тестами
         app.getNavigationHelper().getOrderAdminPage(orderNumber);
 
         // Assert order isn't cancelled
@@ -61,7 +66,7 @@ public class Administration extends TestBase {
     }
 
 
-    @Test(priority = 803)
+    @Test(priority = 703)
     public void cancelTestOrders() throws Exception {
         app.getSessionHelper().cancelAllTestOrders(); // TODO перенести метод в AdministrationHelper
     }
