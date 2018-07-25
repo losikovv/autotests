847eeb1fda94d0a7bf6abeb9b2cbe1bb1f93256b
Stanislav Klimov <stanklimov@yandex.ru>
2018-07-25 20:47:43 +0300

    new test for Kazan

---
+++ web-tests/src/test/java/ru/instamart/autotests/tests/MakeOrders.java
@@ -0,0 +1,58 @@
+package ru.instamart.autotests.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+
+    // Тесты заказов
+
+
+
+public class MakeOrders extends TestBase {
+
+    @BeforeMethod(alwaysRun = true)
+    public void preparing() throws Exception {
+        app.getNavigationHelper().getRetailerPage("metro");
+
+        // авторизуемся, если нужно
+        if(!app.getSessionHelper().isUserAuthorised()) {
+            app.getSessionHelper().doLoginAsAdmin();
+        }
+    }
+
+
+    @Test(
+            description = "Тест заказа в Казани",
+            groups = {"acceptance","regression"},
+            priority = 450
+    )
+    public void makeOrderInKazan(){
+
+        app.getShoppingHelper().changeShippingAddress("Казань, ул Мулланура Вахитова, д 10");
+
+        // идем в чекаут, при необходимости набирая корзину
+        app.getNavigationHelper().getCheckoutPage();
+        if(!app.getCheckoutHelper().isOnCheckout()){
+            app.getShoppingHelper().grabCartWithMinimalOrderSum();
+            app.getShoppingHelper().proceedToCheckout();
+        }
+
+        app.getCheckoutHelper().completeCheckout();
+
+        // Проверяем что заказ оформился и активен
+        Assert.assertTrue(app.getProfileHelper().isOrderActive(),
+                "Can't assert the order is sent & active, check manually\n");
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void cancelLastOrder()throws Exception {
+        app.getProfileHelper().cancelLastOrder();
+        app.getNavigationHelper().getRetailerPage("metro");
+        app.getShoppingHelper().changeShippingAddress("Москва, ул Просторная, д 77");
+    }
+
+}
