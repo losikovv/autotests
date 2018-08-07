fe9e278fcab5278d1a966afd6927a8358c7a1464
Stanislav Klimov <stanklimov@yandex.ru>
2018-08-07 19:21:23 +0300

    test addresses

--- web-tests/src/test/java/ru/instamart/autotests/testdata/Addresses.java
+++ web-tests/src/test/java/ru/instamart/autotests/testdata/Addresses.java
@@ -1,4 +1,43 @@
 package ru.instamart.autotests.testdata;
 
+
+
+    // Тестовые адреса доставки
+
+
+
 public class Addresses {
+
+    public static String get (String type) {
+        final String address;
+        switch (type) {
+
+            case "default":
+                address = "Москва, ул Тестовская, д 1";
+                break;
+            case "non-default":
+                address = "Москва, ул Люсиновская, д 12";
+                break;
+            case "out-of-zone":
+                address = "Москва, ул Лосиноостровская, д 2 стр 1";
+                break;
+            case "office":
+                address = "Москва, пер Казачий 2-й, д 11 стр 2";
+                break;
+
+            case "default-kazan":
+                address = "Казань, ул Мулланура Вахитова, д 10";
+                break;
+            case "non-default-kazan":
+                address = "Казань, ул Танковая, д 30";
+                break;
+            case "out-of-zone-kazan":
+                address = "Казань, ул Беломорская, д 1";
+                break;
+            default: address = null;
+
+        }
+        return address;
+    }
+
 }
