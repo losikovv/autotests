cf6d2e9003da0925eec8241b4352488680aedfb3
Stanislav Klimov <stanklimov@yandex.ru>
2018-08-24 23:35:07 +0300

    STF-987 bugfix test added

--- web-tests/src/test/java/ru/instamart/autotests/tests/Bugfixes.java
+++ web-tests/src/test/java/ru/instamart/autotests/tests/Bugfixes.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 
 
-    // Регрессионные тесты старых багов
+    // Регрессионные тесты старых багов и редких кейсов
     // Все тесты с groups = {"regression"}
 
 
@@ -17,7 +17,7 @@ public class Bugfixes extends TestBase {
     @Test (
             description = "Test description",
             groups = {"regression"},
-            priority = 999
+            priority = 1000
     )
     public void testName(){
         app.getShoppingHelper().searchItem("смысл жизни");
@@ -25,6 +25,23 @@ public class Bugfixes extends TestBase {
         // Проверяем что-нибудь
         Assert.assertTrue(app.getShoppingHelper().isSearchResultsEmpty(),
                 "Result is not expected\n");
+    }
+
+
+    @Test (
+            description = "Тест возможности открыть авторизационную модалку после запроса восстановления пароля",
+            groups = {"regression"},
+            priority = 1001
+    )
+    public void STF987(){
+        app.getNavigationHelper().getBaseUrl();
+        app.getSessionHelper().dropAuth();
+        app.getSessionHelper().recoverPassword("instatestuser@yandex.ru");
+        app.getSessionHelper().closeAuthModal();
+        app.getSessionHelper().openAuthModal();
+
+        Assert.assertFalse(app.getSessionHelper().isRecoverySent(),
+                "Can't open auth modal right after sending password recovery request \n");
     }
 
 
