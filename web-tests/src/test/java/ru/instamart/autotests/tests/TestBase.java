fc0e49d3c7c6d5f59c8454a04108c60c9bd67671
Stanislav Klimov <stanklimov@yandex.ru>
2018-12-14 11:56:06 +0300

    optimize imports

--- web-tests/src/test/java/ru/instamart/autotests/tests/TestBase.java
+++ web-tests/src/test/java/ru/instamart/autotests/tests/TestBase.java
@@ -1,11 +1,9 @@
 package ru.instamart.autotests.tests;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.instamart.autotests.appmanager.ApplicationManager;
-import ru.instamart.autotests.application.Config;
 import ru.instamart.autotests.application.Pages;
 
 
