b8ca3cacac9d57cbaa8b358de80f9213baa80192
Stanislav Klimov <stanklimov@yandex.ru>
2019-01-18 15:52:14 +0300

    fix jivosite methods

--- web-tests/src/test/java/ru/instamart/autotests/appmanager/JivositeHelper.java
+++ web-tests/src/test/java/ru/instamart/autotests/appmanager/JivositeHelper.java
@@ -1,10 +1,8 @@
 package ru.instamart.autotests.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.instamart.autotests.application.Environments;
 import ru.instamart.autotests.application.Elements;
-
 
 
 public class JivositeHelper extends HelperBase {
@@ -18,18 +16,28 @@ public class JivositeHelper extends HelperBase {
 
     /** Открыть чат jivosite */
     public void open() {
-        kraken.perform().click(Elements.Site.Jivosite.openJivosite());
+        if(kraken.detect().isElementDisplayed(Elements.Site.Jivosite.openButton())) {
+            kraken.perform().printMessage("> разворачиваем виджет Jivosite");
+            kraken.perform().click(Elements.Site.Jivosite.openButton());
+            kraken.perform().waitingFor(1); // Ожидание разворачивания виджета Jivosite
+        }
     }
 
     /** Свернуть чат jivosite */
     public void close() {
-        kraken.perform().click(Elements.Site.Jivosite.closeJivosite());
+        if(kraken.detect().isElementDisplayed(Elements.Site.Jivosite.closeButton())) {
+            kraken.perform().printMessage("> сворачиваем виджет Jivosite");
+            kraken.perform().click(Elements.Site.Jivosite.closeButton());
+            kraken.perform().waitingFor(1); // Ожидание сворачивания виджета Jivosite
+        }
     }
 
     /** Отправить сообщение в Jivosite */
-    public void sendMessage() {
-        kraken.perform().click(Elements.Site.Jivosite.openJivosite());
-        kraken.perform().fillField(Elements.Site.Jivosite.fieldJivosite(), "test");
-        kraken.perform().click(Elements.Site.Jivosite.sendMessage());
+    public void sendMessage(String text) {
+        kraken.perform().printMessage("Jivosite");
+        open();
+        kraken.perform().printMessage("> отправляем сообщение: " + text);
+        kraken.perform().fillField(Elements.Site.Jivosite.messageField(), text);
+        kraken.perform().click(Elements.Site.Jivosite.sendMessageButton());
     }
 }
