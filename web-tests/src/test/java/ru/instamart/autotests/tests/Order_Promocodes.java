be3765d3159ac390be5d7c0d20ddcd60b16cc140
Stanislav Klimov <stanklimov@yandex.ru>
2019-03-27 18:44:30 +0300

    New test classes for TO DO

---
+++ web-tests/src/test/java/ru/instamart/autotests/tests/Order_Promocodes.java
@@ -0,0 +1,13 @@
+package ru.instamart.autotests.tests;
+
+// Тесты заказов с промокодами
+
+public class Order_Promocodes extends TestBase {
+
+    // TODO Заказ c промокодом unicorn + проверка в админке что в заказе есть промик
+
+    // TODO Заказ c промокодом testpromo (только STAGE) + проверка в админке что в заказе есть промик + проверка что сработала скидка
+
+    // TODO перенести все тесты из PromoCodes, переписав при необходимости и заведя тестовые промоакции
+
+}
---
+++ web-tests/src/test/java/ru/instamart/autotests/tests/Order_RetailerBonuses.java
@@ -0,0 +1,9 @@
+package ru.instamart.autotests.tests;
+
+// Тесты заказов со всеми бонусными программами ритейлеров
+
+public class Order_RetailerBonuses extends TestBase {
+
+    // TODO Заказ c бонусной картой Вкусвилл + проверка в админке что в заказе есть бонусная программа
+
+}
