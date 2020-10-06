5c6b467770c99c90b3c67b0613c1e5dacf232c96
Vitaliy Losikov <losikov@list.ru>
2020-10-06 12:13:28 +0300

    ATST-248 (#187)
    * shopper properties
    
    * apiV2 refactoring
    
    * apiV2 replacing
    
    * apiV2 replacing
    
    * shopperApi begining

---
+++ web-tests/src/main/java/ru/instamart/api/shopper/objects/Attributes.java
@@ -0,0 +1,35 @@
+package instamart.api.shopper.objects;
+
+import instamart.api.common.BaseObject;
+
+public class Attributes extends BaseObject {
+
+    private String accessToken;
+    private String refreshToken;
+    private String expiresAt;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(String expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+}
