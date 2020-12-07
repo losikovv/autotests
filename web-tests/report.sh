1efe993b711da0917276e3cc1adacfba989f832b
matvey.kuksin <matvey.kuksin@instamart.ru>
2020-12-07 09:54:51 +0300

    Remove experiments

--- web-tests/report.sh
+++ web-tests/report.sh
@@ -9,9 +9,7 @@ PROJECT_ID=$3
 
 SCRIPT_PATH="${BASH_SOURCE[0]}"
 DIR="$(cd "$(dirname "$SCRIPT_PATH")" >/dev/null 2>&1 && pwd)"
-echo "======DEBUG ==>" $DIR
 FILES_TO_SEND=$(ls -dp $DIR/$ALLURE_RESULTS_DIRECTORY/* | grep -v /$)
-echo "======DEBUG ==>" $FILES_TO_SEND
 
 if [ -z "$FILES_TO_SEND" ]; then
   exit 1
@@ -28,10 +26,10 @@ curl -X POST "$ALLURE_SERVER/allure-docker-service/send-results?project_id=$PROJ
 
 
 # Если нужно сгенерировать отчет, нужно отправить запрос на эндпоинт GET /generate-report и выставить >> CHECK_RESULTS_EVERY_SECONDS: NONE в контейнере с отчетами
-#curl -X GET 'http://192.168.88.70:5050/allure-docker-service/generate-report?project_id=default&execution_name=test_exec&execution_from=http://local.com&execution_type=bobobob'
+#curl -X GET 'http://localhost:5050/allure-docker-service/generate-report?project_id=default&execution_name=test_exec&execution_from=http://local.com&execution_type=bobobob'
 echo "------------------GENERATE-REPORT------------------"
 EXECUTION_NAME='Gitlab-CI'
-EXECUTION_FROM='https://gitlab.sbermarket.tech/qa/automag/-/pipelines'
-EXECUTION_TYPE='bamboo'
+EXECUTION_FROM='https://gitlab.sbermarket.tech/qa/automag/'
+EXECUTION_TYPE='Gitlab-Gradle'
 
 RESPONSE=$(curl -X GET "$ALLURE_SERVER/allure-docker-service/generate-report?project_id=$PROJECT_ID&execution_name=$EXECUTION_NAME&execution_from=$EXECUTION_FROM&execution_type=$EXECUTION_TYPE" $FILES)
