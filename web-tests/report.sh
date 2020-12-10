6911521709e7f288a56c8546c47953a8da48c57c
matvey.kuksin <matvey.kuksin@instamart.ru>
2020-12-10 07:52:22 +0300

    Fix for MR

--- web-tests/report.sh
+++ web-tests/report.sh
@@ -7,19 +7,25 @@ ALLURE_SERVER=$2
 # Проект в который будет записываться отчет
 PROJECT_ID=$3
 
+# Получаем путь до баш крипта
 SCRIPT_PATH="${BASH_SOURCE[0]}"
+# Строим полный путь
 DIR="$(cd "$(dirname "$SCRIPT_PATH")" >/dev/null 2>&1 && pwd)"
+# Собираем файлы для отправки
 FILES_TO_SEND=$(ls -dp $DIR/$ALLURE_RESULTS_DIRECTORY/* | grep -v /$)
 
+# Если нет файлов прерываем процесс
 if [ -z "$FILES_TO_SEND" ]; then
   exit 1
 fi
 
+# Строим url с файлами
 FILES=''
 for FILE in $FILES_TO_SEND; do
   FILES+="-F files[]=@$FILE "
 done
 
+# Отправляем через curl запрос на заливку отчета
 set -o xtrace
 echo "------------------SEND-RESULTS------------------"
 curl -X POST "$ALLURE_SERVER/allure-docker-service/send-results?project_id=$PROJECT_ID" -H 'Content-Type: multipart/form-data' $FILES -ik
@@ -31,5 +37,5 @@ echo "------------------GENERATE-REPORT------------------"
 EXECUTION_NAME='Gitlab-CI'
 EXECUTION_FROM='https://gitlab.sbermarket.tech/qa/automag/'
 EXECUTION_TYPE='Gitlab-Gradle'
-
+# Отправляем через curl запрос на генерацию отчета
 RESPONSE=$(curl -X GET "$ALLURE_SERVER/allure-docker-service/generate-report?project_id=$PROJECT_ID&execution_name=$EXECUTION_NAME&execution_from=$EXECUTION_FROM&execution_type=$EXECUTION_TYPE" $FILES)
