8958245118f9a5dbbfb0d693a9783baa0905ce60
matvey.kuksin <matvey.kuksin@instamart.ru>
2020-12-11 11:59:21 +0300

    Fix local test start

--- web-tests/src/main/java/ru/instamart/core/service/BannerService.java
+++ web-tests/src/main/java/ru/instamart/core/service/BannerService.java
@@ -7,14 +7,16 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Objects;
 
 public final class BannerService {
 
     private static final Logger logger = LoggerFactory.getLogger(BannerService.class);
-    private static final String BANNER_URL = "src/test/resources/banner.txt";
+    private static final String BANNER_URL = "banner.txt";
+    private static final String RESOURCE = Objects.requireNonNull(BannerService.class.getClassLoader().getResource(BANNER_URL)).getPath();
 
     public static void printBanner() {
-        try(final BufferedReader in = new BufferedReader(new FileReader(BANNER_URL));) {
+        try(final BufferedReader in = new BufferedReader(new FileReader(RESOURCE))) {
             String line = in.readLine();
             while(line !=null) {
                 System.out.println(line);
