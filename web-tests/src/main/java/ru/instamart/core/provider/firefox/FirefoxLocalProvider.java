9054092fdcb118f0c465d5ab89d2df480b990583
matvey.kuksin <matvey.kuksin@instamart.ru>
2021-02-09 11:39:20 +0300

    Add settings for local firefox

--- web-tests/src/main/java/ru/instamart/core/provider/firefox/FirefoxLocalProvider.java
+++ web-tests/src/main/java/ru/instamart/core/provider/firefox/FirefoxLocalProvider.java
@@ -2,7 +2,9 @@ package instamart.core.provider.firefox;
 
 import instamart.core.provider.AbstractBrowserProvider;
 import instamart.core.util.ProcessUtils;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.util.Optional;
@@ -16,6 +18,22 @@ public final class FirefoxLocalProvider extends AbstractBrowserProvider {
         if (DO_CLEANUP_BEFORE_TEST_RUN) {
             ProcessUtils.cleanProcessByName(BrowserType.FIREFOX);
         }
-        createLocalFirefoxDriver(Optional.empty());
+        final FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        final FirefoxOptions options = new FirefoxOptions();
+        options.setBinary(firefoxBinary);
+        //This is fore resolution
+        options.addArguments("--width=1920");
+        options.addArguments("--height=1080");
+        //This for screenshot
+        options.addArguments("--window-size").addArguments("1920,1080");
+
+        if (System.getProperty("os.name").contains("Mac")) {
+            System.setProperty("webdriver.gecko.driver", "WebDriverMac/geckodriver");
+        } else {
+            System.setProperty("webdriver.gecko.driver", "WebDriverLinux/geckodriver");
+        }
+
+        createLocalFirefoxDriver(Optional.of(options));
     }
 }
