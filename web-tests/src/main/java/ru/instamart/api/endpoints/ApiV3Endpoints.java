6e4f1a31d1b3d93cb64335c58e5e2711723dcb7a
Виталий Лосиков <vitaliy.losikov@instamart.ru>
2021-03-19 07:33:39 +0000

    Первый api v3 тест

---
+++ web-tests/src/main/java/ru/instamart/api/endpoints/ApiV3Endpoints.java
@@ -0,0 +1,5 @@
+package instamart.api.endpoints;
+
+public class ApiV3Endpoints {
+    public static final String STORES = "v3/stores?shipping_method={shippingMethod}";
+}
---
+++ web-tests/src/main/java/ru/instamart/api/requests/v3/StoresRequest.java
@@ -0,0 +1,27 @@
+package instamart.api.requests.v3;
+
+import instamart.api.endpoints.ApiV3Endpoints;
+import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+
+import static instamart.api.requests.InstamartRequestsBase.givenCatch;
+
+public class StoresRequest {
+    /**
+     * Получение списка магазинов
+     */
+    @Step("{method} /" + ApiV3Endpoints.STORES)
+    public static Response GET(String shippingMethod) {
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("lat", "37.445940");
+        requestParams.put("lon", "55.836721");
+        return givenCatch()
+                .contentType(ContentType.JSON)
+                .body(requestParams)
+                .header("Api-Version","3.0")
+                .header("Client-Token","14cd5d341d768bd4926fc9f5ce262094")
+                .get(ApiV3Endpoints.STORES, shippingMethod);
+    }
+}
