350dcf008221345e40c250bd3923c35cd9e5d06c
matvey.kuksin <matvey.kuksin@instamart.ru>
2021-04-09 07:50:01 +0300

    Add new tests for Purchased Products

---
+++ web-tests/src/test/java/ru/instamart/tests/api/v2/endpoints/PurchasedProductsV2Test.java
@@ -0,0 +1,77 @@
+package ru.instamart.tests.api.v2.endpoints;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qase.api.annotation.CaseId;
+import io.restassured.response.Response;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.instamart.api.SessionFactory;
+import ru.instamart.api.common.RestBase;
+import ru.instamart.api.enums.SessionType;
+import ru.instamart.api.requests.v2.PurchasedProductsV2Request;
+import ru.instamart.api.responses.v2.ProductsV2Response;
+import ru.instamart.ui.common.pagesdata.EnvironmentData;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
+import static ru.instamart.api.checkpoints.InstamartApiCheckpoints.*;
+
+@Epic("ApiV2")
+@Feature("Купленные продукты")
+public final class PurchasedProductsV2Test extends RestBase {
+
+    @BeforeClass(alwaysRun = true)
+    @Story("Создание сессии")
+    public void preconditions() {
+        SessionFactory.makeSession(SessionType.APIV2);
+        apiV2.order(
+                SessionFactory.getSession(SessionType.APIV2).getUserData(),
+                EnvironmentData.INSTANCE.getDefaultSid());
+    }
+
+    @AfterClass(alwaysRun = true,
+            description = "Отменяем заказ")
+    public void cleanup() {
+        apiV2.cancelCurrentOrder();
+    }
+
+    @CaseId(266)
+    @Story("Получить список ранее купленных продуктов")
+    @Test(  description = "Существующий sid",
+            groups = {"api-instamart-smoke", "api-instamart-prod"})
+    public void testGetPurchasedProductWithValidSid() {
+        final Response response = PurchasedProductsV2Request.GET(
+                SessionFactory.getSession(SessionType.APIV2).getToken(),
+                EnvironmentData.INSTANCE.getDefaultSid());
+        checkStatusCode200(response);
+        final ProductsV2Response productsV2Response = response.as(ProductsV2Response.class);
+        assertNotNull(productsV2Response, "Вернулся пустой ответ");
+        assertNotNull(productsV2Response.getProducts(), "Список продуктов не вернулся");
+        assertFalse(productsV2Response.getProducts().isEmpty(), "Список продуктов вернулся пустой");
+    }
+
+    @CaseId(267)
+    @Story("Получить список ранее купленных продуктов")
+    @Test(  description = "Несуществующий sid",
+            groups = {"api-instamart-smoke", "api-instamart-prod"})
+    public void testGetPurchasedProductWithInvalidSid() {
+        final Response response = PurchasedProductsV2Request.GET(
+                SessionFactory.getSession(SessionType.APIV2).getToken(),
+                6666666);
+        checkStatusCode404(response);
+    }
+
+    @CaseId(268)
+    @Story("Получить список ранее купленных продуктов")
+    @Test(  description = "Отправка запроса без авторизации",
+            groups = {"api-instamart-smoke", "api-instamart-prod"})
+    public void testGetPurchasedProductWithValidSidAndInvalidAuth() {
+        final Response response = PurchasedProductsV2Request.GET(
+                "invalid_token",
+                EnvironmentData.INSTANCE.getDefaultSid());
+        checkStatusCode401(response);
+    }
+}
