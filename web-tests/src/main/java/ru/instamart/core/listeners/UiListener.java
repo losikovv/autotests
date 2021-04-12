63a8ac54bbf3ac32f1b16e4325086acf5aa8e19d
matvey.kuksin <matvey.kuksin@instamart.ru>
2021-04-12 18:32:37 +0300

    Complete test run when suite finish

--- web-tests/src/main/java/ru/instamart/core/listeners/UiListener.java
+++ web-tests/src/main/java/ru/instamart/core/listeners/UiListener.java
@@ -1,24 +1,27 @@
 package ru.instamart.core.listeners;
 
 import lombok.extern.slf4j.Slf4j;
+import org.testng.*;
 import ru.instamart.core.service.QaseService;
 import io.qase.api.enums.RunResultStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
 
 import java.util.List;
 
 @Slf4j
-public final class UiListener implements ITestListener {
+public final class UiListener implements ITestListener, ISuiteListener {
 
     private final QaseService qaseService;
 
     public UiListener() {
         String projectId = System.getProperty("qase.Project","STF");
         this.qaseService = new QaseService(projectId);
+    }
+
+    @Override
+    public void onStart(ISuite suite) {
+
     }
 
     @Override
@@ -29,13 +32,6 @@ public final class UiListener implements ITestListener {
 
     @Override
     public void onFinish(ITestContext context) {
-        try {
-            this.qaseService.completeTestRun();
-            this.qaseService.deleteOldTestRuns();
-            this.qaseService.deleteOldDefects();
-        } catch (Exception e) {
-            log.error("FATAL: something wrong when try to finish test", e);
-        }
     }
 
     @Override
@@ -59,5 +55,16 @@ public final class UiListener implements ITestListener {
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+    }
+
+    @Override
+    public void onFinish(ISuite suite) {
+        try {
+            this.qaseService.completeTestRun();
+            this.qaseService.deleteOldTestRuns();
+            this.qaseService.deleteOldDefects();
+        } catch (Exception e) {
+            log.error("FATAL: something wrong when try to finish test", e);
+        }
     }
 }
