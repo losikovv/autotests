2a3d09c9a7049a5e4d4277ec1b1feb23a59bb29e
Rozalina Zaleeva <rozalina.zaleeva@instamart.ru>
2021-05-07 17:22:29 +0300

    Добавлен тест на получение всех ролей пользователя

---
+++ web-tests/src/main/java/ru/instamart/api/responses/shopper/admin/RolesSHPResponse.java
@@ -0,0 +1,17 @@
+package ru.instamart.api.responses.shopper.admin;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import ru.instamart.api.objects.shopper.admin.MetaV1;
+import ru.instamart.api.objects.v1.RoleV1;
+import ru.instamart.api.responses.BaseResponseObject;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class RolesSHPResponse extends BaseResponseObject {
+    private List<RoleV1> roles = null;
+    private MetaV1 meta;
+
+}
