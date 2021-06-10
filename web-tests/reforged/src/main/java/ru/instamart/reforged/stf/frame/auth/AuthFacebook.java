cc2487d94947dcea666f0f8a7c69958902392be4
ivan.samarskiy <ivan.samarskiy@instamart.ru>
2021-06-10 18:00:29 +0300

    fix conflicts

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/auth/AuthFacebook.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/auth/AuthFacebook.java
@@ -1,4 +1,28 @@
 package ru.instamart.reforged.stf.frame.auth;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import ru.instamart.reforged.core.component.Element;
+import ru.instamart.reforged.core.component.Input;
+
 public class AuthFacebook {
+
+    private final Input email = new Input(By.xpath("//input[@id='email']"));
+    private final Input password = new Input(By.xpath("//input[@id='pass']"));
+    private final Element login = new Element(By.xpath("//label[@id='loginbutton']"));
+
+    @Step("Ввести email на странице авторизации через FB")
+    public void setEmail(final String text) {
+        email.fill(text);
+    }
+
+    @Step("Ввести пароль на странице авторизации через FB")
+    public void setPassword(final String text) {
+        password.fill(text);
+    }
+
+    @Step("Нажать Вход на странице авторизации через FB")
+    public void clickToLogin() {
+        login.click();
+    }
 }
