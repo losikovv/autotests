6201cb87729aee910753f2aa078c1c42bd9fc290
ivan.samarskiy <ivan.samarskiy@instamart.ru>
2021-06-18 10:24:26 +0300

    added secure pages

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/page/checkout/SecureApprovePage/CP.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/page/checkout/SecureApprovePage/CP.java
@@ -1,4 +1,22 @@
 package ru.instamart.reforged.stf.page.checkout.SecureApprovePage;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import ru.instamart.reforged.core.component.Button;
+import ru.instamart.reforged.core.component.Input;
+
 public class CP {
+
+    private final Input value = new Input(By.xpath("//input[@id='password']"));
+    private final Button submit = new Button(By.xpath("//button[@type='submit']"));
+
+    @Step("Заполнить проверочное значение на странице подтверждения 3ds(CP)")
+    public void fillValue(String data) {
+        value.fill(data);
+    }
+
+    @Step("Заполнить проверочное значение на странице подтверждения 3ds(CP)")
+    public void clickToSubmit(String data) {
+        submit.click();
+    }
 }
--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/page/checkout/SecureApprovePage/Sber.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/page/checkout/SecureApprovePage/Sber.java
@@ -1,4 +1,22 @@
 package ru.instamart.reforged.stf.page.checkout.SecureApprovePage;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import ru.instamart.reforged.core.component.Button;
+import ru.instamart.reforged.core.component.Input;
+
 public class Sber {
+
+    private final Input value = new Input(By.xpath("//input[@id='password']"));
+    private final Button cancel = new Button(By.xpath("//a[text() = 'Отмена']"));
+
+    @Step("Заполнить проверочное значение на странице подтверждения 3ds(sber)")
+    public void fillValue(String data) {
+        value.fill(data);
+    }
+
+    @Step("Нажать отменить на странице подтверждения 3ds(sber)")
+    public void clickToSubmit(String data) {
+        cancel.click();
+    }
 }
