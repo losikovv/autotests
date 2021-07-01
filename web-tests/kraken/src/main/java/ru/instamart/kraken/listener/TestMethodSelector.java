9a88553eb19dea1660dca1a5cfeab7fa097d939b
matvey.kuksin <matvey.kuksin@instamart.ru>
2021-07-01 07:47:31 +0300

    Add skip on and run on annotation

--- web-tests/kraken/src/main/java/ru/instamart/kraken/listener/TestMethodSelector.java
+++ web-tests/kraken/src/main/java/ru/instamart/kraken/listener/TestMethodSelector.java
@@ -3,10 +3,24 @@ package ru.instamart.kraken.listener;
 import org.testng.IMethodSelector;
 import org.testng.IMethodSelectorContext;
 import org.testng.ITestNGMethod;
+import ru.instamart.kraken.testdata.pagesdata.EnvironmentData;
 
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Set;
 
+/**
+ * Селектор методов для запуска или пропуска тестов. Соответственно запускает тесты с аннотацией {@link Run}
+ * или пропускает тесты с аннотацией {@link Skip}
+ * Для применения данного селектора нужно разместить сниппет
+ * <method-selectors>
+ *   <method-selector>
+ *     <selector-class name="ru.instamart.kraken.listener.TestMethodSelector"/>
+ *   </method-selector>
+ * </method-selectors>
+ * в нужных suite, данный блок можно размещать как для всего suite так и для конкретного набора тестов
+ * путем указания внутри блока <suite></suite> или внутри блока <test></test>
+ */
 public final class TestMethodSelector implements IMethodSelector {
 
     @Override
@@ -14,8 +28,36 @@ public final class TestMethodSelector implements IMethodSelector {
         final Method testMethod = method.getConstructorOrMethod().getMethod();
 
         if (isTestMethod && testMethod.isAnnotationPresent(Skip.class)) {
-            context.setStopped(true);
-            return false;
+            final Skip skip = testMethod.getAnnotation(Skip.class);
+            final Set<String> onServer = Set.of(skip.onServer());
+            final Set<String> onTenant = Set.of(skip.onTenant());
+
+            if (onServer.size() == 0 && onTenant.size() == 0) {
+                context.setStopped(true);
+                return false;
+            }
+            if (onServer.contains(EnvironmentData.INSTANCE.getServer())) {
+                context.setStopped(true);
+                return false;
+            }
+            if (onTenant.contains(EnvironmentData.INSTANCE.getTenant())) {
+                context.setStopped(true);
+                return false;
+            }
+        }
+        if (isTestMethod && testMethod.isAnnotationPresent(Run.class)) {
+            final Run run = testMethod.getAnnotation(Run.class);
+            final Set<String> onServer = Set.of(run.onServer());
+            final Set<String> onTenant = Set.of(run.onTenant());
+
+            if (onServer.size() > 0 && !onServer.contains(EnvironmentData.INSTANCE.getServer())) {
+                context.setStopped(true);
+                return false;
+            }
+            if (onTenant.size() > 0 && !onTenant.contains(EnvironmentData.INSTANCE.getTenant())) {
+                context.setStopped(true);
+                return false;
+            }
         }
         return true;
     }
