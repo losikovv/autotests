a0b048e1731acabd99e27aaa0e2dc5493cdcb9d2
Sergey Otryvanov <sergey.otryvanov@instamart.ru>
2021-07-26 08:36:11 +0300

    ATST-579 fix

--- web-tests/kraken/src/main/java/ru/instamart/kraken/helper/PhoneNumberHelper.java
+++ web-tests/kraken/src/main/java/ru/instamart/kraken/helper/PhoneNumberHelper.java
@@ -1,21 +1,25 @@
 package ru.instamart.kraken.helper;
 
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.swing.text.MaskFormatter;
 import java.text.ParseException;
 
+@Slf4j
 public class PhoneNumberHelper {
 
-    private static String phoneMask= "### ###-##-##";
+    private static String phoneMask = "### ###-##-##";
 
-
-    public static String getHumanPhoneNumber(String phoneNumber){
+    public static String getHumanPhoneNumber(@NonNull String phoneNumber) {
         MaskFormatter maskFormatter;
         try {
             maskFormatter = new MaskFormatter(phoneMask);
             maskFormatter.setValueContainsLiteralCharacters(false);
-            return "+7 "+maskFormatter.valueToString(phoneNumber) ;
+            return "+7 " + maskFormatter.valueToString(phoneNumber);
 
         } catch (ParseException e) {
+            log.error("Ошибка валидации номера телефона. ", e);
             e.printStackTrace();
         }
         return null;
