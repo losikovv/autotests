4ae7b873b2c398c7ffd376e64fff68b3599046a5
matvey.kuksin <matvey.kuksin@instamart.ru>
2021-07-30 10:37:51 +0300

    Add description

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/page/user/favorites/UseFavoritesElement.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/page/user/favorites/UseFavoritesElement.java
@@ -13,14 +13,16 @@ public interface UseFavoritesElement {
     AuthoredHeader header = new AuthoredHeader();
     ProductCard productCart = new ProductCard();
 
-    Element emptyFavorites = new Element(By.className("empty-favorites"));
-    ElementCollection favoriteButton = new ElementCollection(By.xpath("//div[@data-qa='addToCart_favorite']"));
-    ElementCollection allFavorites = new ElementCollection(By.xpath("//a[contains(@class, 'favorites__')]"));
-    ElementCollection addToCart = new ElementCollection(By.xpath("//div[@class='cart-actions']"));
-    Button showMore = new Button(By.className("load-more-products"));
+    Element emptyFavorites = new Element(By.className("empty-favorites"), "плейсхолдер пустого списка");
+    ElementCollection favoriteButton = new ElementCollection(By.xpath("//div[@data-qa='addToCart_favorite']"),
+            "список всех кнопок для добавления в избранное");
+    ElementCollection allFavorites = new ElementCollection(By.xpath("//a[contains(@class, 'favorites__')]"), "список всех избранных");
+    ElementCollection addToCart = new ElementCollection(By.xpath("//div[@class='cart-actions']"),
+            "список всех кнопок добавления в корзину");
+    Button showMore = new Button(By.className("load-more-products"), "кнопка Показать еще");
 
-    Element activeElementFilter = new Element(By.xpath("//a[contains(@class,'favorite-list-filter__link--active')]"));
-    Link allGoods = new Link(By.xpath("//a[@data-content='Все товары']"));
-    Link inStock = new Link(By.xpath("//a[@data-content='В наличии']"));
-    Link outOfStock = new Link(By.xpath("//a[@data-content='Нет в наличии']"));
+    Element activeElementFilter = new Element(By.xpath("//a[contains(@class,'favorite-list-filter__link--active')]"), "признак активного фильтра");
+    Link allGoods = new Link(By.xpath("//a[@data-content='Все товары']"), "кнопка Все товары");
+    Link inStock = new Link(By.xpath("//a[@data-content='В наличии']"), "кнопка В наличии");
+    Link outOfStock = new Link(By.xpath("//a[@data-content='Нет в наличии']"), "кнопка Нет в наличии");
 }
