3ddd1efb4ba6171bd8640ddd302866ad8cfdd865
Sergey Otryvanov <sergey.otryvanov@instamart.ru>
2021-08-03 16:38:27 +0300

    ATST-614

--- web-tests/qase-api/src/main/java/io/qase/api/annotation/CaseIDs.java
+++ web-tests/qase-api/src/main/java/io/qase/api/annotation/CaseIDs.java
@@ -2,6 +2,11 @@ package io.qase.api.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * Внимание!!! Использовать с тестами с dataProvider
+ * Запрос к qase.io отправляется по порядку в dataProvider
+ * Порядок caseid должен совпадать в dataProvider
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
