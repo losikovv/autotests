49b549ff98405ddec64c8fc027f144d33a804b4f
matvey.kuksin <matvey.kuksin@instamart.ru>
2021-08-06 10:56:11 +0300

    Fix Crypt encoding and add decode url for ui module

--- web-tests/reforged/src/main/java/ru/instamart/reforged/core/provider/AbstractBrowserProvider.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/core/provider/AbstractBrowserProvider.java
@@ -21,10 +21,8 @@ import ru.instamart.kraken.util.Crypt;
 
 import java.net.URI;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
-import static ru.instamart.kraken.setting.Config.BASIC_TIMEOUT;
 import static ru.instamart.kraken.setting.Config.FULL_SCREEN_MODE;
 
 @Slf4j
