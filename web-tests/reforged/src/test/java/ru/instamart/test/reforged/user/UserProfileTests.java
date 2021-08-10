9bfb9a2047fbc674ad304056528e8ecae59fe760
vladimirWXW <vladimir.hudoykin@instamart.ru>
2021-08-10 14:55:09 +0400

    Тесты профиля

---
+++ web-tests/reforged/src/test/java/ru/instamart/test/reforged/user/UserProfileTests.java
@@ -0,0 +1,10 @@
+package ru.instamart.test.reforged.user;
+
+import org.testng.annotations.BeforeClass;
+import ru.instamart.kraken.setting.Config;
+import ru.instamart.kraken.testdata.Generate;
+import ru.instamart.test.reforged.BaseTest;
+
+public class UserProfileTests extends BaseTest {
+
+}
