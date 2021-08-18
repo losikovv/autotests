dfeefde1972332ad8b1f2bc0956a7ed65141ae21
ivan.samarskiy <ivan.samarskiy@instamart.ru>
2021-08-18 15:43:04 +0300

    delete CheckoutPage, CheckoutCheck, CheckoutElement from frame

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/checkout/CheckoutElement.java
+++
@@ -1,9 +0,0 @@
-package ru.instamart.reforged.stf.frame.checkout;
-
-import org.openqa.selenium.By;
-import ru.instamart.reforged.core.component.Button;
-
-public interface CheckoutElement {
-
-    Button checkoutButton = new Button(By.xpath("//div[@class='checkout-sidebar__content']/descendant::button[@data-qa='checkout_order_button']"));
-}
