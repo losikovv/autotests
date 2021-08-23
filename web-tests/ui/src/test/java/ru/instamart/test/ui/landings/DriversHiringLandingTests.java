5d781e09828e57a1b8e78ea76db7e45d9996aef2
matvey.kuksin <matvey.kuksin@instamart.ru>
2021-08-23 11:01:04 +0300

    Fix locator for landing

--- web-tests/ui/src/test/java/ru/instamart/test/ui/landings/DriversHiringLandingTests.java
+++ web-tests/ui/src/test/java/ru/instamart/test/ui/landings/DriversHiringLandingTests.java
@@ -1,28 +1,27 @@
 package ru.instamart.test.ui.landings;
 
-import ru.instamart.ui.checkpoint.BaseUICheckpoints;
-import ru.instamart.kraken.testdata.lib.Pages;
-import ru.instamart.ui.Elements;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.qase.api.annotation.CaseId;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.instamart.kraken.testdata.lib.Pages;
 import ru.instamart.test.ui.TestBase;
+import ru.instamart.ui.Elements;
+import ru.instamart.ui.checkpoint.BaseUICheckpoints;
 
-public class DriversHiringLandingTests extends TestBase {
-    BaseUICheckpoints baseChecks = new BaseUICheckpoints();
-    @BeforeMethod(alwaysRun = true,
-            description ="Выполняем шаги предусловий для теста")
-    public void beforeTest() {
-        kraken.get().page(Pages.Landings.driversHiring());
-    }
+@Epic("STF UI")
+@Feature("Проверка лендингов")
+public final class DriversHiringLandingTests extends TestBase {
+
+    private final BaseUICheckpoints baseChecks = new BaseUICheckpoints();
 
     @CaseId(1686)
     @Test(
             description = "Тест лендинга найма водителей Сбермаркета",
-
             groups = {"testing","sbermarket-Ui-smoke"}
     )
     public void successValidateSbermarketDriversHiringLanding() {
+        kraken.get().page(Pages.Landings.driversHiring());
         baseChecks.checkPageIsAvailable();
         baseChecks.checkIsElementPresent(Elements.Landings.driversHiringLanding.DriverJobForm.submitButton());
         baseChecks.checkIsElementPresent(Elements.Landings.driversHiringLanding.DriverJobForm.citySelector());
