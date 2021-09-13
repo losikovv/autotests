47c770c6a472ae4de023f65ad2ae3c60ca5d0734
yantuzlukov <yan.tuzlukov@instamart.ru>
2021-09-13 20:17:19 +0700

    test fix after review

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/auth/auth_modal/AuthModal.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/auth/auth_modal/AuthModal.java
@@ -54,6 +54,7 @@ public final class AuthModal implements Close, AuthModalCheck {
 
     @Step("Заполнить поле с телефоном {phone}")
     public void fillPhone(final String phone) {
+        phoneField.hoverAndClick();
         phoneField.fill(phone);
     }
 
