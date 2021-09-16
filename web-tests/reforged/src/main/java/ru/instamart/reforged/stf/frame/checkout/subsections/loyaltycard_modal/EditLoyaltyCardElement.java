efaec44a76816c076dca75741569344b053c2ea3
yantuzlukov <yan.tuzlukov@instamart.ru>
2021-09-16 15:18:11 +0700

    CheckoutBonusesTests done

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/checkout/subsections/loyaltycard_modal/EditLoyaltyCardElement.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/checkout/subsections/loyaltycard_modal/EditLoyaltyCardElement.java
@@ -5,6 +5,5 @@ import ru.instamart.reforged.core.component.Input;
 
 public interface EditLoyaltyCardElement {
 
-    Input value = new Input(By.xpath("//div[@class='modal-form']//input[contains(@class, 'checkout-input')]"));
-
+    Input value = new Input(By.xpath("//div[@class='modal-form']//input[contains(@class, 'checkout-input')]"), "Инпут ввода номера карты лояльности на модальном окне");
 }
