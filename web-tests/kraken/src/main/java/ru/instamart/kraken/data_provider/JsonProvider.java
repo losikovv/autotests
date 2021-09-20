94976f8a08edaf609a032b6888463fccbdead713
matvey.kuksin <matvey.kuksin@instamart.ru>
2021-09-20 08:08:16 +0300

    Add custom JsonDeserializer

--- web-tests/kraken/src/main/java/ru/instamart/kraken/data_provider/JsonProvider.java
+++ web-tests/kraken/src/main/java/ru/instamart/kraken/data_provider/JsonProvider.java
@@ -1,8 +1,8 @@
 package ru.instamart.kraken.data_provider;
 
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.DataProvider;
-import ru.instamart.kraken.util.FileUtils;
+import ru.instamart.utils.FileUtils;
 import ru.instamart.utils.Mapper;
 
 import java.lang.reflect.Method;
@@ -10,17 +10,17 @@ import java.lang.reflect.Method;
 import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
+@Slf4j
 public final class JsonProvider {
 
-    @SneakyThrows
     @DataProvider(parallel = true, name = "json")
     public static Object[][] parseJson(final Method method) {
-        final JsonDataProvider dataProvider = method.getAnnotation(JsonDataProvider.class);
+        var dataProvider = method.getAnnotation(JsonDataProvider.class);
         if (isNull(dataProvider)) {
             throw new RuntimeException("Missing JsonDataProvider annotation");
         }
 
-        final DataList<?> dataList = Mapper.INSTANCE.jsonToObject(FileUtils.getJson(dataProvider.path()), dataProvider.type());
+        var dataList= Mapper.INSTANCE.jsonToObject(FileUtils.getJson(dataProvider.path(), method.getDeclaringClass()), dataProvider.type());
         return nonNull(dataList) ? dataList.getData().stream().map(data -> new Object[]{data}).toArray(Object[][]::new) : new Object[0][];
     }
 }
