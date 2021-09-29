d9897a7633b0e1d136c738b0fa6c0290e9aa24c9
Sergey Otryvanov <sergey.otryvanov@sbermarket.ru>
2021-09-29 15:13:07 +0300

    fix

--- web-tests/kraken/src/main/java/ru/instamart/kraken/config/EnvironmentProperties.java
+++ web-tests/kraken/src/main/java/ru/instamart/kraken/config/EnvironmentProperties.java
@@ -19,10 +19,22 @@ public final class EnvironmentProperties {
     public static String TENANT;
     @Config(configName = NAME, fieldName = "server", defaultValue = "")
     public static String SERVER;
+
+    @Config(configName = NAME, fieldName = "basicUrl", defaultValue = "")
+    private static String BASIC_URL;
+    @Config(configName = NAME, fieldName = "httpAuth", defaultValue = "", crypted = true)
+    private static String HTTP_AUTH;
+
+    @Config(configName = NAME, fieldName = "shopperUrl", defaultValue = "")
+    private static String SHOPPER_URL;
     @Config(configName = NAME, fieldName = "defaultSid", defaultValue = "")
     public static int DEFAULT_SID;
     @Config(configName = NAME, fieldName = "defaultShopperSid", defaultValue = "")
     public static int DEFAULT_SHOPPER_SID;
+
+    @Config(configName = NAME, fieldName = "protocol", defaultValue = "https", args = "protocol")
+    private static String PROTOCOL;
+
     @Config(configName = NAME, fieldName = "dbUrl", defaultValue = "")
     public static String DB_URL;
     @Config(configName = NAME, fieldName = "dbUsername", defaultValue = "", crypted = true)
@@ -33,33 +45,10 @@ public final class EnvironmentProperties {
     public static String K8S_NAME_SPACE;
     @Config(configName = NAME, fieldName = "k8sLabelSelector", defaultValue = "")
     public static String K8S_LABEL_SELECTOR;
-    @Config(configName = NAME, fieldName = "basicUrl", defaultValue = "")
-    private static String BASIC_URL;
-    @Config(configName = NAME, fieldName = "httpAuth", defaultValue = "", crypted = true)
-    private static String HTTP_AUTH;
-    @Config(configName = NAME, fieldName = "shopperUrl", defaultValue = "")
-    private static String SHOPPER_URL;
-    @Config(configName = NAME, fieldName = "protocol", defaultValue = "https", args = "protocol")
-    private static String PROTOCOL;
 
-    public static String getDomainName(String url) {
-        try {
-            URI uri = new URI(url);
-            return uri.getHost();
-        } catch (URISyntaxException e) {
-            log.debug("Domain parse error: {}", e.getMessage());
-        }
-        return url;
-    }
+
 
     public static class Env {
-
-        public static String ENV_NAME = TENANT + "-" + SERVER;
-        public static String FULL_SITE_URL = PROTOCOL + "://" + BASIC_URL + "/";
-        public static String FULL_SITE_URL_WITH_BASIC_AUTH = PROTOCOL + "://" + HTTP_AUTH + BASIC_URL + "/";
-        public static String FULL_ADMIN_URL = FULL_SITE_URL + "admin/";
-        public static String FULL_ADMIN_URL_WITH_BASIC_AUTH = FULL_SITE_URL_WITH_BASIC_AUTH + "admin/";
-        public static String FULL_SHOPPER_URL = PROTOCOL + "://" + SHOPPER_URL + "/";
 
         //TODO: Немножк костылей. Насколько мне известно эти урлы можно без костылей достать из переменных которые в основной пайпе от разработки
         static {
@@ -76,6 +65,23 @@ public final class EnvironmentProperties {
                     SHOPPER_URL = "shp" + customBasicUrl.substring(3);
                 } else SHOPPER_URL = "shp-0.k-stage.sbermarket.tech";
             }
+        }
+
+        public static String ENV_NAME = TENANT + "-" + SERVER;
+        public static String FULL_SITE_URL = PROTOCOL + "://" + BASIC_URL + "/";
+        public static String FULL_SITE_URL_WITH_BASIC_AUTH = PROTOCOL + "://" + HTTP_AUTH + BASIC_URL + "/";
+        public static String FULL_ADMIN_URL = FULL_SITE_URL + "admin/";
+        public static String FULL_ADMIN_URL_WITH_BASIC_AUTH = FULL_SITE_URL_WITH_BASIC_AUTH + "admin/";
+        public static String FULL_SHOPPER_URL = PROTOCOL + "://" + SHOPPER_URL + "/";
+
+        public static String getDomainName(String url) {
+            try {
+                URI uri = new URI(url);
+                return uri.getHost();
+            } catch (URISyntaxException e) {
+                log.debug("Domain parse error: {}", e.getMessage());
+            }
+            return url;
         }
     }
 }
