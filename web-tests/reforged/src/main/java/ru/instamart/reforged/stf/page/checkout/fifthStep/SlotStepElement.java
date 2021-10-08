8fcc0b4b1c0262d1f0d12a4f7419c9cfd934dd59
yantuzlukov <yan.tuzlukov@instamart.ru>
2021-10-08 14:54:29 +0700

    fix

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/page/checkout/fifthStep/SlotStepElement.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/page/checkout/fifthStep/SlotStepElement.java
@@ -9,7 +9,7 @@ public interface SlotStepElement {
 
     Element slotsSpinner = new Element(By.xpath("//div[@class = 'windows-selector-content']//div[contains(@class, 'Spinner')]"));
     Element firstActiveSlot = new Element(By.xpath("//div[(contains(@id, 'deliveryDay'))]//div[@class='windows-selector-item']"));
-    ElementCollection activeSlots = new ElementCollection(By.xpath("//div[(contains(@id, 'deliveryDay'))]//div[@class='windows-selector-item']"));
+    ElementCollection activeSlots = new ElementCollection(By.xpath("//div[(contains(@id, 'deliveryDay'))]//div[@class='windows-selector-item']"), "Коллекция активных слотов");
     Button choseAnotherTimeButton = new Button(By.xpath("//button[contains(@class,'delivery_time')]"),"Выбрать другое время");
     Element firstActiveSlotTime = new Element(By.xpath("//div[(contains(@id, 'deliveryDay'))]//div[@class='windows-selector-item']//span[(contains(@class, 'interval'))]"));
     Button submit = new Button(By.xpath("//button[@data-qa='checkout_delivery_submit_button']"));
