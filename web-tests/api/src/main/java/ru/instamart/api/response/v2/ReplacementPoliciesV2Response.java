d48ad5015982d7b9d269355a56618ecc3baff04d
Sergey Otryvanov <sergey.otryvanov@sbermarket.ru>
2021-10-11 17:26:00 +0300

    ATST-752

---
+++ web-tests/api/src/main/java/ru/instamart/api/response/v2/ReplacementPoliciesV2Response.java
@@ -0,0 +1,17 @@
+package ru.instamart.api.response.v2;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import ru.instamart.api.model.BaseObject;
+import ru.instamart.api.model.v2.ReplacementPoliciesItemV2;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ReplacementPoliciesV2Response extends BaseObject {
+
+    @JsonProperty("replacement_policies")
+    private List<ReplacementPoliciesItemV2> replacementPolicies;
+}
