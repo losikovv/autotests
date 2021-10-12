f02f6dae687aa502d53dc419377d5bffeac0cc02
Sergey Otryvanov <sergey.otryvanov@sbermarket.ru>
2021-10-12 13:09:17 +0300

    ATST-753

---
+++ web-tests/api/src/main/java/ru/instamart/api/request/v2/OnboardingV2PagesV2Request.java
@@ -0,0 +1,14 @@
+package ru.instamart.api.request.v2;
+
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import ru.instamart.api.endpoint.ApiV2EndPoints;
+import ru.instamart.api.request.ApiV2RequestBase;
+
+public class OnboardingV2PagesV2Request extends ApiV2RequestBase {
+
+    @Step("{method} /" + ApiV2EndPoints.Onboarding.ONBOARDING_V2_PAGES)
+    public static Response GET() {
+        return givenWithSpec().get(ApiV2EndPoints.Onboarding.ONBOARDING_V2_PAGES);
+    }
+}
