2ee8be5286d6df2ddfd0b7a4afe02b6f6358e293
Виталий Лосиков <vitaliy.losikov@instamart.ru>
2021-10-13 17:29:04 +0300

    salut token test

---
+++ web-tests/grpc/src/test/java/ru/instamart/test/content/salut_token/SalutTokenTest.java
@@ -0,0 +1,42 @@
+package ru.instamart.test.content.salut_token;
+
+import io.qameta.allure.Epic;
+import io.qase.api.annotation.CaseId;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.instamart.grpc.common.GrpcBase;
+import salut_token.SalutToken;
+import salut_token.TokenGeneratorGrpc;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
+
+@Epic("Salut Token Microservice")
+@Slf4j
+public class SalutTokenTest extends GrpcBase {
+    private TokenGeneratorGrpc.TokenGeneratorBlockingStub client;
+
+    @BeforeClass(alwaysRun = true)
+    public void createClient() {
+        channel = grpcStep.createChannel("paas-content-salut-token.k-stage.sbmt.io", 443);
+        client = TokenGeneratorGrpc.newBlockingStub(channel);
+    }
+
+    @CaseId(1)
+    @Test(  description = "Get token",
+            groups = "grpc-salut-token")
+    public void getToken() {
+        var request = SalutToken.TokenRequest.newBuilder()
+                .setInstallationId("258b347d-b978-4a75-8f02-407d43a932af").build();
+
+        grpcStep.showRequestInAllure(request);
+
+        var response = client.getToken(request);
+
+        grpcStep.showResponseInAllure(response);
+
+        assertNotNull(response.getToken(), "Не вернулся токен");
+        assertFalse(response.getToken().isEmpty(), "Вернулся пустой токен");
+    }
+}
