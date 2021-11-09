2105635aa8173ca8a3af528980d34aa69f89cb11
Сергей Отрыванов <sergey.otryvanov@instamart.ru>
2021-11-09 11:53:23 +0000

    ATST-838

--- report.sh
+++ report.sh
@@ -13,7 +13,10 @@ SCRIPT_PATH="${BASH_SOURCE[0]}"
 # Строим полный путь
 DIR="$(cd "$(dirname "$SCRIPT_PATH")" >/dev/null 2>&1 && pwd)"
 # Собираем файлы для отправки
-FILES_TO_SEND=$(ls -dp $DIR/$ALLURE_RESULTS_DIRECTORY/* | grep -v /$)
+#FILES_TO_SEND=$(find $DIR/$ALLURE_RESULTS_DIRECTORY/ -type f | grep -v /$)
+shopt -s nullglob
+FILES_TO_SEND=($DIR/$ALLURE_RESULTS_DIRECTORY/*)
+echo "Array size: ${#FILES_TO_SEND[*]}"  # Выводим размер массива
 
 # Если нет файлов прерываем процесс
 if [ -z "$FILES_TO_SEND" ]; then
@@ -21,17 +24,22 @@ if [ -z "$FILES_TO_SEND" ]; then
   exit 0
 fi
 
-# Строим url с файлами
+
 FILES=''
-for FILE in $FILES_TO_SEND; do
-  FILES+="-F files[]=@$FILE "
+#set -o xtrace
+for FILE in ${!FILES_TO_SEND[*]}; do
+  # Строим url с файлами
+  FILES+="-F files[]=@${FILES_TO_SEND[$FILE]} "
+   if [ $(($FILE % 1000)) -eq 0 ]; then
+      # Отправляем через curl запрос на заливку отчета
+      #set -o xtrace
+      echo "------------------SEND-RESULTS------------------"
+      sendResultsResponse=$(curl  -u $WEB_LOGIN:$WEB_PASSWORD -X POST $ALLURE_SERVER"/allure-docker-service/send-results?project_id=$PROJECT_ID" -H 'Content-Type: multipart/form-data' $FILES -ik)
+      echo "$sendResultsResponse"
+      FILES=''
+    fi
 done
 
-# Отправляем через curl запрос на заливку отчета
-#set -o xtrace
-echo "------------------SEND-RESULTS------------------"
-sendResultsResponse=$(curl  -u $WEB_LOGIN:$WEB_PASSWORD -X POST $ALLURE_SERVER"/allure-docker-service/send-results?project_id=$PROJECT_ID" -H 'Content-Type: multipart/form-data' $FILES -ik)
-echo "$sendResultsResponse"
 # Если нужно сгенерировать отчет, нужно отправить запрос на эндпоинт GET /generate-report и выставить >> CHECK_RESULTS_EVERY_SECONDS: NONE в контейнере с отчетами
 #curl -X GET 'http://localhost:5050/allure-docker-service/generate-report?project_id=default&execution_name=test_exec&execution_from=http://local.com&execution_type=bobobob'
 echo "------------------GENERATE-REPORT------------------"
