004bdf168ba6588abff63129a319e9aea25dd0e0
Анна Яргина <anna.yargina@sbermarket.ru>
2021-11-16 15:48:07 +0000

    ATST-840

---
+++ web-tests/api/src/test/java/ru/instamart/test/api/v2/endpoints/OnboardingV2PagesV2Test.java
@@ -0,0 +1,36 @@
+package ru.instamart.test.api.v2.endpoints;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qase.api.annotation.CaseId;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import ru.instamart.api.common.RestBase;
+import ru.instamart.api.model.v2.OnboardingPageV2;
+import ru.instamart.api.request.v2.OnboardingV2PagesV2Request;
+import ru.instamart.api.response.v2.OnboardingPagesV2Response;
+import ru.instamart.jdbc.dao.OnboardingV2PagesDao;
+
+import java.util.List;
+
+import static ru.instamart.api.checkpoint.BaseApiCheckpoints.checkFieldIsNotEmpty;
+import static ru.instamart.api.checkpoint.BaseApiCheckpoints.compareTwoObjects;
+import static ru.instamart.api.checkpoint.StatusCodeCheckpoints.checkStatusCode200;
+
+@Epic("ApiV2")
+@Feature("Получение списка страниц онбординга (Сториз)")
+public class OnboardingV2PagesV2Test extends RestBase {
+
+    @CaseId(821)
+    @Story("Экраны онбординга")
+    @Test(description = "Автоматическое получение экранов онбординга при старте приложения",
+            groups = {"api-instamart-regress"})
+    public void getOnboardingPages() {
+        Response response = OnboardingV2PagesV2Request.GET();
+        checkStatusCode200(response);
+        List<OnboardingPageV2> onboardingPages = response.as(OnboardingPagesV2Response.class).getOnboardingPages();
+        checkFieldIsNotEmpty(onboardingPages, "экраны онбординга");
+        compareTwoObjects(onboardingPages.size(), OnboardingV2PagesDao.INSTANCE.getCount());
+    }
+}
---
+++ web-tests/jdbc/src/main/java/ru/instamart/jdbc/dao/OnboardingV2PagesDao.java
@@ -0,0 +1,55 @@
+package ru.instamart.jdbc.dao;
+
+import ru.instamart.jdbc.entity.OnboardingV2PagesEntity;
+import ru.instamart.jdbc.util.ConnectionMySQLManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class OnboardingV2PagesDao implements Dao<Long, OnboardingV2PagesEntity>{
+
+    public static final OnboardingV2PagesDao INSTANCE = new OnboardingV2PagesDao();
+    private final String SELECT_SQL = "SELECT %s FROM onboarding_v2_pages";
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public OnboardingV2PagesEntity save(OnboardingV2PagesEntity ticket) {
+        return null;
+    }
+
+    @Override
+    public void update(OnboardingV2PagesEntity ticket) {
+
+    }
+
+    @Override
+    public Optional<OnboardingV2PagesEntity> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<OnboardingV2PagesEntity> findAll() {
+        return null;
+    }
+
+    public int getCount() {
+        int resultCount = 0;
+        try (Connection connect = ConnectionMySQLManager.get();
+             PreparedStatement preparedStatement = connect.prepareStatement(String.format(SELECT_SQL, "COUNT(*)") + " WHERE active = 1")) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            resultCount = resultSet.getInt("count(*)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultCount;
+    }
+}
