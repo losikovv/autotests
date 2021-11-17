cd5a26a36715fd5e531b9d34b42904acb225eab9
Сергей Отрыванов <sergey.otryvanov@instamart.ru>
2021-11-17 08:33:43 +0000

    ATST-828

--- web-tests/grpc/src/test/java/ru/instamart/test/content/catalog/NavigationTest.java
+++ web-tests/grpc/src/test/java/ru/instamart/test/content/catalog/NavigationTest.java
@@ -1,5 +1,6 @@
 package ru.instamart.test.content.catalog;
 
+import io.grpc.StatusRuntimeException;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qase.api.annotation.CaseId;
@@ -8,6 +9,7 @@ import navigation.Navigation;
 import navigation.NavigationServiceGrpc;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import ru.instamart.grpc.common.GrpcBase;
 import ru.instamart.grpc.common.GrpcHosts;
 
@@ -25,8 +27,8 @@ public class NavigationTest extends GrpcBase {
         client = NavigationServiceGrpc.newBlockingStub(channel);
     }
 
-    @CaseId(1)
-    @Test(  groups = {"grpc-product-hub"},
+    @CaseId(150)
+    @Test(groups = {"grpc-product-hub"},
             description = "Проверка построения дерева продуктов")
     public void getMenuTree() {
         var request = Navigation.GetMenuTreeRequest
@@ -42,5 +44,128 @@ public class NavigationTest extends GrpcBase {
 
         var categories = response.getCategoriesList();
         assertTrue(grpc.findCategoryById(categories, "900000"), "Не вернулась 900000 категория");
+    }
+
+
+    @CaseId(151)
+    @Test(groups = {"grpc-product-hub"},
+            description = "Проверка построения дерева продуктов без поля \"store_id\"",
+            expectedExceptions = StatusRuntimeException.class,
+            expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: required field \"store_id\" is not specified")
+    public void getMenuTreeWithout() {
+        var request = Navigation.GetMenuTreeRequest
+                .newBuilder()
+                .setTenantId("sbermarket")
+                .setTreeDepth(5)
+                .build();
+        client.getMenuTree(request);
+    }
+
+    @CaseId(152)
+    @Test(groups = {"grpc-product-hub"},
+            description = "Получение хлебных крошек по ID категории.")
+    public void getBreadcrumbsCategoryID() {
+        var request = Navigation.GetBreadcrumbsByCategoryIDRequest.newBuilder()
+                .setCategoryId("131")
+                .build();
+        var response = client.getBreadcrumbsByCategoryID(request);
+        final SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(response.getBreadcrumbs(0).getId(), "131", "Категория отличается");
+        softAssert.assertEquals(response.getBreadcrumbs(0).getName(), "Вода, соки, напитки", "Наименование категории не совпадает");
+        softAssert.assertEquals(response.getBreadcrumbs(0).getPermalink(), "voda-soki-napitki-new", "Permalink не совпадает");
+        softAssert.assertAll();
+    }
+
+    @CaseId(153)
+    @Test(groups = {"grpc-product-hub"},
+            description = "Получение хлебных крошек по ID категории.",
+            expectedExceptions = StatusRuntimeException.class,
+            expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: required field \"category_id\" is not specified")
+    public void getBreadcrumbsWithoutCategoryID() {
+        var request = Navigation.GetBreadcrumbsByCategoryIDRequest.newBuilder()
+                .setCategoryId("")
+                .build();
+        client.getBreadcrumbsByCategoryID(request);
+    }
+
+
+    @CaseId(154)
+    @Test(groups = {"grpc-product-hub"},
+            description = "Проверка построения дерева категорий")
+    public void getCategoryTree() {
+        var request = Navigation.GetCategoryTreesRequest.newBuilder()
+                .setCategoryId("131")
+                .build();
+        var response = client.getCategoryTrees(request);
+        final SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(response.getCategories(0).getId(), "131", "Категория отличается");
+        softAssert.assertEquals(response.getCategories(0).getName(), "Вода, соки, напитки", "Наименование категории не совпадает");
+        softAssert.assertEquals(response.getCategories(0).getStatus(), Navigation.Status.ENABLE, "status не совпадает");
+        softAssert.assertTrue(response.getCategories(0).getChildrenList().size() > 0, "Children null");
+        softAssert.assertAll();
+    }
+
+    @CaseId(155)
+    @Test(groups = {"grpc-product-hub"},
+            description = "Проверка построения дерева категорий с пустым значением у поля \"category_id\"")
+    public void emptyCategoryID() {
+        var request = Navigation.GetCategoryTreesRequest.newBuilder()
+                .setCategoryId("")
+                .build();
+        var response = client.getCategoryTrees(request);
+        assertTrue(response.getCategoriesCount() > 0, "Вернулся пустой массив категорий");
+    }
+
+    @CaseId(224)
+    @Test(groups = {"grpc-product-hub"},
+            description = "Проверка построения дерева продуктов без поля \"tenant_id\"",
+            expectedExceptions = StatusRuntimeException.class,
+            expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: required field \"tenant_id\" is not specified")
+    public void withoutTenantID() {
+        var request = Navigation.GetMenuTreeRequest.newBuilder()
+                .setStoreId("1")
+                .setTreeDepth(5)
+                .build();
+        client.getMenuTree(request);
+    }
+
+    @CaseId(225)
+    @Test(groups = {"grpc-product-hub"},
+            description = "Проверка построения дерева продуктов с пустым значением в поле \"store_id\"",
+            expectedExceptions = StatusRuntimeException.class,
+            expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: required field \"store_id\" is not specified")
+    public void emptyStoreID() {
+        var request = Navigation.GetMenuTreeRequest.newBuilder()
+                .setStoreId("")
+                .setTenantId("sbermarket")
+                .setTreeDepth(5)
+                .build();
+        client.getMenuTree(request);
+    }
+
+    @CaseId(226)
+    @Test(groups = {"grpc-product-hub"},
+            description = "Проверка построения дерева продуктов с пустым значением в поле \"tenant_id\"",
+            expectedExceptions = StatusRuntimeException.class,
+            expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: required field \"tenant_id\" is not specified")
+    public void emptyTenantID() {
+        var request = Navigation.GetMenuTreeRequest.newBuilder()
+                .setStoreId("1")
+                .setTenantId("")
+                .setTreeDepth(5)
+                .build();
+        client.getMenuTree(request);
+    }
+
+    @CaseId(227)
+    @Test(groups = {"grpc-product-hub"},
+            description = "Проверка построения дерева продуктов без поля \"tree_depth\"")
+    public void withoutTreeDepth() {
+        var request = Navigation.GetMenuTreeRequest.newBuilder()
+                .setStoreId("1")
+                .setTenantId("sbermarket")
+                .build();
+        var response = client.getMenuTree(request);
+        assertTrue(response.getCategoriesCount() > 0, "Вернулся пустой массив категорий");
     }
 }
--- web-tests/grpc/src/test/java/ru/instamart/test/content/catalog/ShelfTest.java
+++ web-tests/grpc/src/test/java/ru/instamart/test/content/catalog/ShelfTest.java
@@ -1,10 +1,14 @@
 package ru.instamart.test.content.catalog;
 
+import io.grpc.StatusRuntimeException;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qase.api.annotation.CaseId;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import ru.instamart.grpc.common.GrpcBase;
 import ru.instamart.grpc.common.GrpcHosts;
 import shelf.ShelfOuterClass;
@@ -22,17 +26,116 @@ public class ShelfTest extends GrpcBase {
         client = ShelfServiceGrpc.newBlockingStub(channel);
     }
 
-    @Test(  description = "Get shelf by category id",
+    @Story("Проверка построения полок")
+    @CaseId(182)
+    @Test(description = "Проверка построения полок по категории продукта",
             groups = "grpc-product-hub")
-    public void getShelfByCategoryId() {
+    public void checkingConstructionShelvesByProductCategory() {
         var request = ShelfOuterClass.GetShelfByCategoryIDRequest
                 .newBuilder()
-                .setCategoryId("116")
-                .setProductsLimit(20)
-                .setStoreId("11")
+                .setCategoryId("122")
+                .setProductsLimit(1)
+                .setStoreId("1")
                 .setTenantId("sbermarket")
                 .build();
 
         var response = client.getShelfByCategoryID(request);
+
+        final SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(response.getCategoriesList().size() > 0, "Категории товаров не вернулись");
+        softAssert.assertEquals(response.getShelves(0).getCategoryId(), "123", "Категория товара не совпадает");
+        softAssert.assertEquals(response.getShelves(0).getCategoryPermalink(), "miaso-ptitsa-new/miaso", "category_permalink не совпадает");
+        softAssert.assertTrue(response.getShelves(0).getProductCount() > 0, "product_count is null");
+        softAssert.assertTrue(response.getShelves(0).getProductsList().size() > 0, "products вернулся пустым");
+        softAssert.assertAll();
+    }
+
+    @Story("Проверка построения полок")
+    @CaseId(257)
+    @Test(description = "Проверка построения полок по категории продукта с пустым значением поля \"category_id\"",
+            groups = "grpc-product-hub")
+    public void emptyCategoryID() {
+        var request = ShelfOuterClass.GetShelfByCategoryIDRequest.newBuilder()
+                .setCategoryId("")
+                .setProductsLimit(1)
+                .setStoreId("1")
+                .setTenantId("sbermarket")
+                .build();
+
+        var response = client.getShelfByCategoryID(request);
+        final SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(response.getCategoriesList().size() > 0, "Категории товаров не вернулись");
+        softAssert.assertTrue(response.getShelves(0).getProductCount() > 0, "product_count is null");
+        softAssert.assertTrue(response.getShelves(0).getProductsList().size() > 0, "products вернулся пустым");
+        softAssert.assertAll();
+    }
+
+
+    @Story("Проверка построения полок")
+    @CaseId(258)
+    @Test(description = "Проверка построения полок по категории продукта с пустым полем \"store_id\"",
+            groups = "grpc-product-hub",
+            expectedExceptions = StatusRuntimeException.class,
+            expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: GetShelfByCategoryID: rpc error: code = InvalidArgument desc = required field \"store_id\" is not specified")
+    public void emptyStoreID() {
+        var request = ShelfOuterClass.GetShelfByCategoryIDRequest.newBuilder()
+                .setCategoryId("122")
+                .setProductsLimit(1)
+                .setStoreId("")
+                .setTenantId("sbermarket")
+                .build();
+
+        client.getShelfByCategoryID(request);
+    }
+
+    @Story("Проверка построения полок")
+    @CaseId(259)
+    @Test(description = "Проверка построения полок по категории продукта без передачи поля \"store_id\"",
+            groups = "grpc-product-hub",
+            expectedExceptions = StatusRuntimeException.class,
+            expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: GetShelfByCategoryID: rpc error: code = InvalidArgument desc = required field \"store_id\" is not specified")
+    public void withoutStoreID() {
+        var request = ShelfOuterClass.GetShelfByCategoryIDRequest.newBuilder()
+                .setCategoryId("122")
+                .setProductsLimit(1)
+                .setTenantId("sbermarket")
+                .build();
+
+        client.getShelfByCategoryID(request);
+    }
+
+    @Story("Проверка построения полок")
+    @CaseId(260)
+    @Test(description = "Проверка построения полок по категории продукта с пустым полем \"tenant_id\"",
+            groups = "grpc-product-hub",
+            expectedExceptions = StatusRuntimeException.class,
+            expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: GetShelfByCategoryID: rpc error: code = InvalidArgument desc = required field \"tenant_id\" is not specified")
+    public void emptyTenantID() {
+        var request = ShelfOuterClass.GetShelfByCategoryIDRequest.newBuilder()
+                .setCategoryId("122")
+                .setProductsLimit(1)
+                .setStoreId("1")
+                .setTenantId("")
+                .build();
+
+        client.getShelfByCategoryID(request);
+    }
+
+
+    @Story("Проверка построения полок")
+    @CaseId(261)
+    @Test(description = "Проверка построения полок по категории продукта без поля \"tenant_id\"",
+            groups = "grpc-product-hub",
+            expectedExceptions = StatusRuntimeException.class,
+            expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: GetShelfByCategoryID: rpc error: code = InvalidArgument desc = required field \"tenant_id\" is not specified")
+    public void withoutTenantID() {
+        var request = ShelfOuterClass.GetShelfByCategoryIDRequest.newBuilder()
+                .setCategoryId("122")
+                .setProductsLimit(1)
+                .setStoreId("1")
+                .setTenantId("")
+                .build();
+
+        client.getShelfByCategoryID(request);
     }
 }
