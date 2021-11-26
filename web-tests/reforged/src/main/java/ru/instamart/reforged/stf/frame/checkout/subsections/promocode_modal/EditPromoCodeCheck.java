1c5a2ca2bd38ea966bc168323cd3c537dff83706
yantuzlukov <yan.tuzlukov@instamart.ru>
2021-11-26 23:32:13 +0700

    fix tests

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/checkout/subsections/promocode_modal/EditPromoCodeCheck.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/checkout/subsections/promocode_modal/EditPromoCodeCheck.java
@@ -1,4 +1,19 @@
 package ru.instamart.reforged.stf.frame.checkout.subsections.promocode_modal;
 
-public interface EditPromoCodeCheck extends EditPromoCodeElement {
+import io.qameta.allure.Step;
+import ru.instamart.reforged.core.Check;
+
+import static ru.instamart.reforged.core.Kraken.waitAction;
+
+public interface EditPromoCodeCheck extends EditPromoCodeElement, Check {
+
+    @Step("Проверяем, что алерт первого заказа отображается")
+    default void checkFirstOrderAlertVisible() {
+        waitAction().shouldBeVisible(firstPromoAlert);
+    }
+
+    @Step("Проверяем, что алерт несуществующего промо отображается")
+    default void checkNonExistAlertVisible() {
+        waitAction().shouldBeVisible(nonExistPromoAlert);
+    }
 }
