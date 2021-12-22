a7f077f2bb9f443eaf3301f686ce72a2cce8cb69
yantuzlukov <yan.tuzlukov@instamart.ru>
2021-12-22 22:33:43 +0700

    CheckoutBonusesTests extend

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/checkout/subsections/loyaltycard_modal/EditLoyaltyCardCheck.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/checkout/subsections/loyaltycard_modal/EditLoyaltyCardCheck.java
@@ -1,6 +1,7 @@
 package ru.instamart.reforged.stf.frame.checkout.subsections.loyaltycard_modal;
 
 import io.qameta.allure.Step;
+import org.testng.Assert;
 
 import static ru.instamart.reforged.core.Kraken.waitAction;
 
@@ -10,5 +11,20 @@ public interface EditLoyaltyCardCheck extends EditLoyaltyCardElement {
     default void checkModalWindow() {
         waitAction().shouldBeVisible(modal);
         waitAction().shouldNotBeAnimated(modal);
+    }
+
+    @Step("Проверяем, что показывается алерт пустого инпута карты")
+    default void checkEmptyCardNumberAlert() {
+        waitAction().shouldBeVisible(emptyCardNumberAlert);
+    }
+
+    @Step("Проверяем, что показывается алерт некорректного инпута карты")
+    default void checkWrongCardNumberAlert() {
+        waitAction().shouldBeVisible(wrongCardNumberAlert);
+    }
+
+    @Step("Проверяем, что в инпуте карты лояльности прописано значение {0}")
+    default void checkCardNumberInputValue(final String expValue) {
+        Assert.assertEquals(value.getValue(), expValue);
     }
 }
