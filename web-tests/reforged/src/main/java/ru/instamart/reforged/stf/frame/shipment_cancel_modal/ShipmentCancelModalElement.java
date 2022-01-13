cfb9251436f5f7513c2ff1f3600f194e0a970666
yantuzlukov <yan.tuzlukov@instamart.ru>
2022-01-13 23:08:36 +0700

    tests fix, logic improve, locators fix

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/shipment_cancel_modal/ShipmentCancelModalElement.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/shipment_cancel_modal/ShipmentCancelModalElement.java
@@ -9,5 +9,4 @@ public interface ShipmentCancelModalElement {
     Element shipmentCancelModal =  new Element(By.xpath("//div[@data-qa='user-shipment-cancel-modal']"), "окно отмены заказа");
     Button accept = new Button(By.xpath("//button[@data-qa='user-shipment-cancel-modal-btn-cancel']"), "кнопка 'Да, отменить'");
     Button decline = new Button(By.xpath("//button[@data-qa='user-shipment-cancel-modal-btn-dismiss']"), "кнопка 'Не отменять'");
-
 }
