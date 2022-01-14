48881de6059e4b346775064dac8ce8e05a4aaf56
yantuzlukov <yan.tuzlukov@instamart.ru>
2022-01-14 12:35:00 +0700

    tests fix, logic improve, locators fix

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/shipment_cancel_modal/ShipmentCancelModalCheck.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/shipment_cancel_modal/ShipmentCancelModalCheck.java
@@ -1,15 +1,18 @@
 package ru.instamart.reforged.stf.frame.shipment_cancel_modal;
 
+import io.qameta.allure.Step;
 import ru.instamart.reforged.core.Check;
 
 import static ru.instamart.reforged.core.Kraken.waitAction;
 
 public interface ShipmentCancelModalCheck extends ShipmentCancelModalElement, Check {
 
+    @Step("Проверяем что моадльное окно отмены заказа показано")
     default void shipmentCancelModalVisible() {
         waitAction().shouldBeVisible(shipmentCancelModal);
     }
 
+    @Step("Проверяем что моадльное окно отмены заказа не видно")
     default void shipmentCancelModalNotVisible() {
         waitAction().shouldNotBeVisible(shipmentCancelModal);
     }
