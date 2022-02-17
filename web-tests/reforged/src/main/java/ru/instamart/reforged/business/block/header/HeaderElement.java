84cff48549ca2b26b6818efd0e689f9d62e19e91
Сергей Стрекалов <sergey.strekalov@sbermarket.ru>
2022-02-17 09:25:28 +0300

    ATST-1892 fix

--- web-tests/reforged/src/main/java/ru/instamart/reforged/business/block/header/HeaderElement.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/business/block/header/HeaderElement.java
@@ -3,6 +3,7 @@ package ru.instamart.reforged.business.block.header;
 import org.openqa.selenium.By;
 import ru.instamart.reforged.business.drawer.cart.account_menu.AccountMenu;
 import ru.instamart.reforged.business.frame.TransferCartModal;
+import ru.instamart.reforged.business.frame.store_selector.StoreSelector;
 import ru.instamart.reforged.core.component.Button;
 import ru.instamart.reforged.core.component.Element;
 
@@ -10,12 +11,14 @@ public interface HeaderElement {
 
     AccountMenu accountMenu = new AccountMenu();
     TransferCartModal transferCartModal = new TransferCartModal();
+    StoreSelector storeSelectorDrawer = new StoreSelector();
 
     Element header = new Element(By.xpath("//header"), "Контейнер для хедера");
 
     Button selectAddress = new Button(By.xpath("//button[@data-qa='select-button']"), "Кнопка 'Указать адрес'");
     Element enteredAddress = new Element(By.xpath("//span[@data-qa='current-ship-address']"), "Лэйбл отображающий введенный адрес в шапке");
     Button login = new Button(By.xpath("//button[contains(.,'Войти')]"), "Кнопка 'Войти'");
+    Button storeSelector = new Button(By.xpath("//button[@data-qa='open-store-selector-button']"), "Кнопка выбора магазина");
     Button profile = new Button(By.xpath("//button[@data-qa='profile-button_button']"), "Кнопка профиль пользователя в хэдере");
     Element cartNotification = new Element(By.xpath("//div[@class='notification']"), "Алерт добавления товара в корзину");
     Button cart = new Button(By.xpath("//button[@data-qa='open-cart-button']"), "Кнопка 'Корзина'");
