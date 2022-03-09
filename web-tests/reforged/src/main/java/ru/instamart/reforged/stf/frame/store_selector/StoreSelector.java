20e3d0669003b2e978542dc8792668c6525ac395
Сергей Стрекалов <sergey.strekalov@sbermarket.ru>
2022-03-09 09:54:52 +0300

    B2B current fixes

--- web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/store_selector/StoreSelector.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/stf/frame/store_selector/StoreSelector.java
@@ -1,6 +1,7 @@
 package ru.instamart.reforged.stf.frame.store_selector;
 
 import io.qameta.allure.Step;
+import ru.instamart.reforged.core.Kraken;
 import ru.instamart.reforged.stf.frame.Close;
 
 public class StoreSelector implements StoreSelectorCheck, Close {
@@ -13,6 +14,11 @@ public class StoreSelector implements StoreSelectorCheck, Close {
     @Step("Выбрать второй магазин в списке")
     public void clickToSecondStoreCard() {
         secondStoreCard.click();
+    }
+
+    @Step("Выбираем магазин с Sid = '{storeSid}'")
+    public void clickToStoreWithSid(final int storeSid){
+        storeBySid.click(storeSid);
     }
 
     @Step("Нажать Изменить адресс")
