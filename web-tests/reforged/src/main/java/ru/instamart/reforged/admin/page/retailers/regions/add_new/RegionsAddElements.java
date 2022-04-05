4251fb5c59b05aab61caf3daa501007c385a6cc2
yantuzlukov <yan.tuzlukov@instamart.ru>
2022-04-05 23:54:24 +0700

    smsSettingsPageValidate() fix, successCreateNewRetailerRegion() fix

--- web-tests/reforged/src/main/java/ru/instamart/reforged/admin/page/retailers/regions/add_new/RegionsAddElements.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/admin/page/retailers/regions/add_new/RegionsAddElements.java
@@ -7,7 +7,7 @@ import ru.instamart.reforged.core.component.Input;
 
 public interface RegionsAddElements {
 
-    Element addNewRegionModal = new Element(By.xpath("//div[@data-qa='operational_zone_edit_drawer']"), "Модальное окно");
+    Element addNewRegionModal = new Element(By.xpath("//div[@data-qa='operational_zone_edit_drawer']"), "Модальное окно создания региона");
     Input newRegionName = new Input(By.xpath("//input[@data-qa='operational_zone_edit_name']"), "Инпут для ввода имени нового региона");
     Button newRegionCreateButton = new Button(By.xpath("//button[@data-qa='operationalZone_edit_submit_edit_btn']"), "Кнопка подтверждения создания нового региона");
 }
