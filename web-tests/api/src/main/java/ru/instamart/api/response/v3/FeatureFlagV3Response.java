d33ffdd2903709b8e1204f573ef0a67a32b21baf
Виталий Лосиков <vitaliy.losikov@instamart.ru>
2022-04-18 19:09:05 +0000

    ATST-1981 (тесты фича флагов)

---
+++ web-tests/api/src/main/java/ru/instamart/api/response/v3/FeatureFlagV3Response.java
@@ -0,0 +1,16 @@
+package ru.instamart.api.response.v3;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.imifou.jsonschema.module.addon.annotation.JsonSchema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import ru.instamart.api.model.v3.FeatureFlagV3;
+import ru.instamart.api.response.BaseResponseObject;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class FeatureFlagV3Response extends BaseResponseObject {
+    @JsonSchema(required = true)
+    @JsonProperty("feature_flag")
+    private FeatureFlagV3 featureFlag;
+}
