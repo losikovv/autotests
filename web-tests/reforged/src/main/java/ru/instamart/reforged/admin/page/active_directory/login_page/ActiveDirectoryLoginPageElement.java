5d8764ea023ab02ef655ddaf1c26c6ca2bb5607f
yantuzlukov <yan.tuzlukov@instamart.ru>
2022-08-11 22:12:48 +0700

    keycloak ci fix

--- web-tests/reforged/src/main/java/ru/instamart/reforged/admin/page/active_directory/login_page/ActiveDirectoryLoginPageElement.java
+++ web-tests/reforged/src/main/java/ru/instamart/reforged/admin/page/active_directory/login_page/ActiveDirectoryLoginPageElement.java
@@ -13,7 +13,7 @@ public interface ActiveDirectoryLoginPageElement {
             "//span[contains(text(),'Неверный идентификатор пользователя или пароль') or contains(text(),'Incorrect user ID or password')]"),
             "Алерт неверного пароля или логина");
     Element wrongLoginFormatAlert = new Element(By.xpath(
-            "//span[contains(text(),'Введите идентификатор пользователя в формате') or contains(text(),'Enter your user ID in format')]"),
+            "//span[contains(text(),'Введите идентификатор пользователя в формате') or contains(text(),'Enter your user ID in the format')]"),
             "Алерт неверного пароля или логина");
 
 }
