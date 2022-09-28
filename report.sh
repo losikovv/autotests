e98276a19864c2e1b6457b1c514076f9de4ff1f1
Отрыванов Сергей Владимирович <sergey.otryvanov@instamart.ru>
2022-09-28 07:43:58 +0000

    ATST-2410

--- report.sh
+++ report.sh
@@ -39,4 +39,12 @@ EXECUTION_TYPE='Gitlab-Gradle'
 # Отправляем через curl запрос на генерацию отчета
 generateResponse=$(curl -u $WEB_LOGIN:$WEB_PASSWORD -X GET $ALLURE_SERVER"/allure-docker-service/generate-report?project_id=$PROJECT_ID&execution_name=$EXECUTION_NAME&execution_from=$EXECUTION_FROM&execution_type=$EXECUTION_TYPE" $FILES)
 echo "$generateResponse"
+
+allureReport=$(sed -E 's/.*"report_url":"?([^,"]*)"?.*/\1/' <<<"$generateResponse")
+
+echo "------------------allure------------------"
+export ALLURE_REPORT_URL="${allureReport}"
+echo "ALLURE_REPORT_URL=$ALLURE_REPORT_URL" >> .env
+echo "ALLURE_REPORT_URL=$ALLURE_REPORT_URL"
+
 rm -rf /tmp/results.zip
