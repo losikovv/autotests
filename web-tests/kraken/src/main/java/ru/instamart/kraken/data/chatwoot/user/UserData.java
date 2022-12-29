07ea35c157f21f116eefe66a8a5ab0e15c252d2b
Сергей Стрекалов <sergey.strekalov@sbermarket.ru>
2022-12-29 09:02:17 +0300

    ATST-2534

--- web-tests/kraken/src/main/java/ru/instamart/kraken/data/chatwoot/user/UserData.java
+++ web-tests/kraken/src/main/java/ru/instamart/kraken/data/chatwoot/user/UserData.java
@@ -1,41 +1,12 @@
 package ru.instamart.kraken.data.chatwoot.user;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.ToString;
 
-@AllArgsConstructor
 @Data
+@Builder
 public final class UserData {
 
     private String email;
     private String password;
-
-    public static UserDataBuilder builder() {
-        return new UserDataBuilder();
-    }
-
-    @ToString
-    public static final class UserDataBuilder {
-        private String email;
-        private String password;
-
-        UserDataBuilder() {
-        }
-
-        public UserDataBuilder email(final String email) {
-            this.email = email;
-            return this;
-        }
-
-        public UserDataBuilder password(final String password) {
-            this.password = password;
-            return this;
-        }
-
-        public UserData build() {
-            return new UserData(email, password);
-        }
-    }
-
 }
--- web-tests/kraken/src/main/java/ru/instamart/kraken/data/chatwoot/user/UserManager.java
+++ web-tests/kraken/src/main/java/ru/instamart/kraken/data/chatwoot/user/UserManager.java
@@ -3,30 +3,21 @@ package ru.instamart.kraken.data.chatwoot.user;
 import lombok.extern.slf4j.Slf4j;
 import ru.instamart.kraken.common.Crypt;
 
-import static java.util.Objects.isNull;
-
 @Slf4j
 public final class UserManager {
 
-    private static UserData defaultUser;
-
     public static UserData getOperatorUser() {
-        if (isNull(defaultUser)) {
-            defaultUser = UserData.builder()
-                    .email(Crypt.INSTANCE.decrypt("7xiUwnaxUQYLiK4zTxUk5p53zTRPovGV8Eq+ghTBuKY="))
-                    .password(Crypt.INSTANCE.decrypt("5AWeCqqme8bpV4dmDSBiNQ=="))
-                    .build();
-        }
-        return defaultUser;
+        return UserData.builder()
+                .email(Crypt.INSTANCE.decrypt("7xiUwnaxUQYLiK4zTxUk5p53zTRPovGV8Eq+ghTBuKY="))
+                .password(Crypt.INSTANCE.decrypt("5AWeCqqme8bpV4dmDSBiNQ=="))
+                .build();
+
     }
 
     public static UserData getSVCCUser() {
-        if (isNull(defaultUser)) {
-            defaultUser = UserData.builder()
-                    .email(Crypt.INSTANCE.decrypt("OYr+/o671sXcGBt3USKQ1OqSRFzZ1Ti9gyXcSrtqnp0="))
-                    .password(Crypt.INSTANCE.decrypt("dxsncs6r90niphxhnTTO4w=="))
-                    .build();
-        }
-        return defaultUser;
+        return UserData.builder()
+                .email(Crypt.INSTANCE.decrypt("OYr+/o671sXcGBt3USKQ1OqSRFzZ1Ti9gyXcSrtqnp0="))
+                .password(Crypt.INSTANCE.decrypt("dxsncs6r90niphxhnTTO4w=="))
+                .build();
     }
 }
